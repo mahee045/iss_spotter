92ab3bd529fd3e94332422879665f5aa8d542d43
Maheerah Anwar <maheerahanwar@gmail.com>
2024-11-17 12:56:53 -0500

    long and lat

--- index.js
+++ index.js
@@ -9,3 +9,19 @@
 
 // console.log('It worked! Returned IP:' , ip);
 // });
+
+// part 2 
+
+
+// Test the function with a sample IP
+
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
--- iss.js
+++ iss.js
@@ -8,19 +8,61 @@ const needle = require('needle');
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
+const fetchMyIP = function (callback) {
+  const url = 'https://api.ipify.org?format=json';
 
-const fetchMyIP = function(callback) {
-  needle.get('https://api.ipify.org?format=json', (error, response, body) => {
-    if (error) return callback(error, null);
+  needle.get(url, (error, response, body) => {
+    if (error) {
+      callback(`Network error: ${error.message}`, null);
+      return;
+    }
 
     if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      callback(
+        `Status Code ${response.statusCode} when fetching IP: ${response.body}`,
+        null
+      );
       return;
     }
 
     const ip = body.ip;
+    if (!ip) {
+      callback('Failed to retrieve IP address.', null);
+      return;
+    }
+
     callback(null, ip);
   });
 };
 
-module.exports = { fetchMyIP };
+/**
+ * Makes a single API request to retrieve the latitude and longitude for a given IP address.
+ * Input:
+ *   - ip (string): The IP address to look up.
+ *   - callback (function): A callback to handle the response or error.
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - An object with latitude and longitude (null if error). Example: { latitude: '49.27670', longitude: '-123.13000' }
+ */
+const fetchCoordsByIP = function (ip, callback) {
+  const url = `http://ipwho.is/${ip}`;
+
+  needle.get(url, (error, response, body) => {
+    if (error) {
+      callback(`Network error: ${error.message}`, null);
+      return;
+    }
+
+    if (!body.success) {
+      const message = `Failed to retrieve coordinates for IP ${ip}. Message: ${body.message}`;
+      callback(Error(message), null);
+      return;
+    }
+
+    const latitude = body.latitude;
+    const longitude = body.longitude;
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
